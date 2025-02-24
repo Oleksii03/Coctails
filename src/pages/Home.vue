ab90d9f0eea6b8df6a872108de7c5955f5ae8362
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-02-24 19:35:07 +0200

    add-select

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { ref } from "vue";
 import AppLayout from "@/components/AppLayout.vue";
 import { useRootStore } from "@/stores/root";
+import { storeToRefs } from "pinia";
 
 const rootStore = useRootStore();
-
 rootStore.getIngredients();
+
+const { ingredients } = storeToRefs(rootStore);
+const ingredient = ref(null);
 </script>
 
 <template>
@@ -13,6 +17,23 @@ rootStore.getIngredients();
       <div class="info">
         <h1 class="title">Choose your drink</h1>
         <div class="line"></div>
+
+        <div class="select-wrapper">
+          <el-select
+            v-model="ingredient"
+            placeholder="Choose main ingredient"
+            size="large"
+            class="select"
+          >
+            <el-option
+              v-for="item in ingredients"
+              :key="item.strIngredient1"
+              :label="item.strIngredient1"
+              :value="item.strIngredient1"
+              class="option"
+            />
+          </el-select>
+        </div>
       </div>
     </div>
   </AppLayout>
@@ -30,5 +51,21 @@ rootStore.getIngredients();
 .info {
   padding: 80px 0;
   text-align: center;
+}
+
+.select-wrapper {
+  margin-top: 42px;
+}
+.select {
+  width: 220px;
+}
+
+.el-select-dropdown,
+.el-popper,
+.el-scrollbar,
+.el-scrollbar__view {
+  color: $bgColor !important;
+  padding: 5px !important;
+  background-color: $bgColor !important;
 }
 </style>
