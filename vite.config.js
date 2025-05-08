e947e658f2f3a3207a39fc56005574d69b62b02e
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2025-05-08 19:29:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from "vite";
-// import vue from "@vitejs/plugin-vue";
-// import vueDevTools from "vite-plugin-vue-devtools";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: "/Coctails",
-
-//   plugins: [vue(), vueDevTools()],
-//   resolve: {
-//     alias: {
-//       "@": fileURLToPath(new URL("./src", import.meta.url)),
-//       "@assets": path.resolve(__dirname, "src/assets"),
-//       "@styles": path.resolve(__dirname, "src/styles"),
-//       "@js": path.resolve(__dirname, "src/js"),
-//     },
-//   },
-// });
 
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
